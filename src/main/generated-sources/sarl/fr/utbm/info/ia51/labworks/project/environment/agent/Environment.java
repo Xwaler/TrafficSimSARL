69cd2fe106bfe0c3ad240f1a59ea586c92649666
TheauZatti <50621116+TheauZatti@users.noreply.github.com>
2021-04-09 17:33:42 +0200

    project package

---
+++ src/main/generated-sources/sarl/fr/utbm/info/ia51/labworks/project/environment/agent/Environment.java
@@ -0,0 +1,33 @@
+package fr.utbm.info.ia51.labworks.project.environment.agent;
+
+import io.sarl.lang.annotation.SarlElementType;
+import io.sarl.lang.annotation.SarlSpecification;
+import io.sarl.lang.annotation.SyntheticMember;
+import io.sarl.lang.core.Agent;
+import io.sarl.lang.core.BuiltinCapacitiesProvider;
+import io.sarl.lang.core.DynamicSkillProvider;
+import java.util.UUID;
+import javax.inject.Inject;
+
+@SarlSpecification("0.11")
+@SarlElementType(19)
+@SuppressWarnings("all")
+public class Environment extends Agent {
+  @SyntheticMember
+  public Environment(final UUID parentID, final UUID agentID) {
+    super(parentID, agentID);
+  }
+  
+  @SyntheticMember
+  @Inject
+  @Deprecated
+  public Environment(final BuiltinCapacitiesProvider provider, final UUID parentID, final UUID agentID) {
+    super(provider, parentID, agentID);
+  }
+  
+  @SyntheticMember
+  @Inject
+  public Environment(final UUID parentID, final UUID agentID, final DynamicSkillProvider skillProvider) {
+    super(parentID, agentID, skillProvider);
+  }
+}
